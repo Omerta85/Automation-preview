8a599d5ef8f7b3796a28d3a6becfb7955fa87d80
Omerta85 <666Omerta@gmail.com>
2024-02-10 17:22:11 +0200

    Initial commit

---
+++ src/test/java/lesson1/FirstTest.java
@@ -0,0 +1,14 @@
+package lesson1;
+
+
+import org.base.BaseTest;
+import org.testng.annotations.Test;
+
+
+public class FirstTest extends BaseTest {
+    @Test
+    public void test(){
+        System.out.println("test");
+    }
+
+}
---
+++ src/test/java/lesson1/SecondTest.java
@@ -0,0 +1,15 @@
+package lesson1;
+
+import org.base.BaseTest;
+import org.testng.annotations.*;
+
+public class SecondTest extends BaseTest {
+//    @BeforeMethod
+//    public void beforeMethodTest(){
+//        System.out.println("This method works before every methods");
+//    }
+    @Test
+    public void test(){
+        System.out.println("Our second test");
+    }
+}
