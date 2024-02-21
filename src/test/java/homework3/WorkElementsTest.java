74c21779ecb74bc8850bb4a01d9a8025f95d2822
Omerta85 <666Omerta@gmail.com>
2024-02-21 16:36:29 +0200

    Lesson 3

--- src/test/java/homework3/WorkElementsTest.java
+++ src/test/java/homework3/WorkElementsTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 public class WorkElementsTest  extends BaseTest{
@@ -22,15 +23,27 @@ public class WorkElementsTest  extends BaseTest{
 
         // Проводимо ітерацію по кожному товару
         for (SelenideElement item : itemsList) {
+            // Зберігаємо назву товару
+            String itemName = item.$(By.className("inventory_item_name")).text();
+
             // Клікаємо на елемент, щоб відкрити сторінку товару
             item.click();
+
+            // Перевіряємо, чи відкривається правильна сторінка товару
+            $(By.className("inventory_details_name")).shouldHave(text(itemName));
+            // Або $(By.xpath("//div[text()='" + itemName + "']")).shouldBe(visible);
 
             // Клікаємо на кнопку "Add to cart"
             $(By.xpath("//button[text()='Add to cart']")).click();
 
+            // Перевіряємо, чи додається товар до кошика
+            $(By.className("shopping_cart_badge")).shouldHave(text(String.valueOf(itemsList.indexOf(item) + 1)));
+
             // Повертаємося назад до списку товарів
             $(By.xpath("//button[text()='Back to products']")).click();
+
+            // Перевіряємо, чи зберігається кошик
+            $(By.className("shopping_cart_badge")).shouldHave(text(String.valueOf(itemsList.indexOf(item) + 1)));
         }
     }
-
 }
